cbaffa0ab84c51bbbf66823cb5672d9642d33308
HienNguyen03 <lily31392@gmail.com>
2017-07-31 20:25:30 +0300

    Sign up action

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -14,13 +14,30 @@ const renderField = ({ input, label, type, meta: { touched, error } }) => (
 )
 
 class Signup extends Component {
-  render () {
+  handleFormSubmit (formProps) {
+    //console.log(formProps);
+    //call action here
+    this.props.signupUser(formProps);
+  }
 
+  renderAlert () {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Opp! </strong>{this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
+  render () {
+    const {handleSubmit} = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
           <Field name="email" type="text" component={renderField} label="Email"/>
           <Field name="password" type="password" component={renderField} label="Password"/>
           <Field name="passwordConfirm" type="password" component={renderField} label="Confirm Password"/>
+          {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign up!</button>
       </form>
     );
@@ -46,6 +63,12 @@ function validate (formProps) {
   }
   return errors;
 }
+
+function mapStateToProps (state) {
+  return {errorMessage: state.auth.error};
+}
+
+Signup = connect(mapStateToProps, actions)(Signup);
 
 export default reduxForm({
   form: 'signup',
